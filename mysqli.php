14b036c06affe77c281f2f06208e3f47f4b48a66
lyon <lyondhill@gmail.com>
2012-09-20 14:56:38 -0600

    stupid

--- mysqli.php
+++ mysqli.php
@@ -1,11 +1,11 @@
 <?php
-$con = mysqli_connect($_SERVER['DB1_HOST'],$_SERVER['DB1_USER'],$_SERVER['DB1_PASS']);
+$con = mysqli_connect($_SERVER['DATABASE1_HOST'],$_SERVER['DATABASE1_USER'],$_SERVER['DATABASE1_PASS']);
 if (!$con)
   {
   die('Could not connect: ' . mysqli_error());
   }
   
-mysqli_select_db($con, $_SERVER['DB1_NAME']);
+mysqli_select_db($con, $_SERVER['DATABASE1_NAME']);
 
 $result1 = mysqli_query($con, "SELECT value FROM core_config_data WHERE path = 'web/cookie/cookie_lifetime' AND scope = 'default' AND scope_id = 0");
 $result2 = mysqli_query($con, "SELECT value FROM core_config_data WHERE path='atb/stores/store_ip'");
--- mysqlp.php
+++ mysqlp.php
@@ -1,10 +1,10 @@
 <?php
-$con = mysql_pconnect($_SERVER['DB1_HOST'],$_SERVER['DB1_USER'],$_SERVER['DB1_PASS']);
+$con = mysql_pconnect($_SERVER['DATABASE1_HOST'],$_SERVER['DATABASE1_USER'],$_SERVER['DATABASE1_PASS']);
 if (!$con)
   {
   die('Could not connect: ' . mysql_error());
   }
-mysql_select_db($_SERVER['DB1_NAME'], $con);
+mysql_select_db($_SERVER['DATABASE1_NAME'], $con);
 $result1 = mysql_query("SELECT value FROM core_config_data WHERE path = 'web/cookie/cookie_lifetime' AND scope = 'default' AND scope_id = 0");
 $result2 = mysql_query("SELECT value FROM core_config_data WHERE path='atb/stores/store_ip'");
 echo 'Test Succeeded';
