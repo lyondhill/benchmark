dc398f880c60827d9597b89e927a684c477815a2
lyon <lyondhill@gmail.com>
2012-09-21 09:02:13 -0600

    asdf

--- redis.php
+++ redis.php
@@ -1,8 +1,8 @@
 <?php
 
 $con = new Redis();
-// $con->connect($_SERVER['CACHE2_HOST'], $_SERVER['CACHE2_PORT']);
-$con->connect('127.0.0.1', 6379);
+$con->connect($_SERVER['CACHE2_HOST'], $_SERVER['CACHE2_PORT']);
+// $con->connect('127.0.0.1', 6379);
 $con->set('x', 'y');
 if ($con->get('x') === 'y') {
   echo 'Test Succeeded';
